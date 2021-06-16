7bc64d9ae411d1cb8cf849370913c36106835e88
sozdilan <dilan.soz@hotmail.com>
2021-06-17 01:07:03 +0300

    update

--- App.js
+++ App.js
@@ -9,177 +9,68 @@
 //AIzaSyAWUYge6_XjqqxENGxDaqNmjCYCUeQ7nrU
 
 import React from 'react';
-import MapView,{ PROVIDER_GOOGLE,Marker,Polygon,Circle} from 'react-native-maps';
-import Geolocation from '@react-native-community/geolocation'
-import {Alert,PermissionsAndroid,Platform,StyleSheet,Text,View,} from 'react-native';
+import { View,Text,SafeAreaView, Alert } from 'react-native';
+import DocumantationMain from './Documentation';
 
-import MapViewDirections from 'react-native-maps-directions';
+import {Marker} from 'react-native-maps';
+import MapView from "react-native-map-clustering";
+import axios from 'axios';
 
 
 export default class Main extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      latitude:'',
-      longitude:'',
-      loading:false
+      events:[],
+      loading:true
     }
   }
-
-  componentDidMount=async()=> {
-    if(Platform.OS=='android'){
-      const response=await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,{
-        'title':'MapsAndGeo',
-        'message':'Konumunuzu istiyoruz'
-      });
-    }else{
-      Geolocation.requestAuthorization();
-    }
-    /*
-    Geolocation.getCurrentPosition(
-        position=>{
-            const{coords:{latitude,longitude}}=position;
-            this.setState({latitude,longitude})  
-        },error=>{
-            console.log(error);
-        }
-    );*/
-
-    this.watchId=Geolocation.watchPosition(
-      position=>{
-        const {coords:{latitude,longitude}}=position;
-        this.setState({latitude,longitude,loading:true})
-      },
-      error=>{
-        console.log(error);
-      }
-    )
+  componentDidMount(){
+    axios.get(`https://eonet.sci.gsfc.nasa.gov/api/v2.1/events`)
+    .then((res)=>{
+      this.setState({
+        events:res.data.events,
+        loading:false
+      })
+    }).catch((error)=>Alert.alert(error))
 
   }
-  componentWillUnmount=()=>{
-    Geolocation.clearWatch(this.watchId);
-  }
+
   render(){
-    const{latitude,longitude,loading}=this.state;
-    console.log(latitude,longitude)
+    const {loading,events}=this.state;
     return(
-      <View style={styles.container}>
-        {(loading) ?
-         <MapView
-         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-         style={styles.map}
-         region={{
-           latitude:Number(latitude),
-           longitude:Number(longitude),
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121
-         }}
-         //showsUserLocation={true}
-         >
-
-           <Marker coordinate={{latitude,longitude}}/>
-         </MapView>
-         :
-         <View><Text>Konum Alınıyor.</Text></View>
-        }
-      </View>
+      //<DocumantationMain/>
+      <SafeAreaView style={{flex:1}}>
+        {loading ? <View style={{flex:1,justifyContent:'center',alignItems:'center'}}><Text>Yükleniyor..</Text></View>  : 
+        <MapView
+        initialRegion={{
+          latitude:52.5,
+          longitude:19.2,
+          latitudeDelta:8.5,
+          longitudeDelta:8.5
+        }}
+        style={{
+          height:'100%'
+        }}
+        >
+          {
+                events.map(item=>{
+                  if(typeof item.geometries[0].coordinates[0]!='object'){
+                    return <Marker 
+                    title={item.title}
+                    description={item.description}
+                    coordinate={{
+                      latitude:item.geometries[0].coordinates[1],
+                      longitude:item.geometries[0].coordinates[0]
+                    }}/>
+                  }  
+                })
+          }
+        </MapView>
+      }
+      </SafeAreaView>
     )
   }
 }
-
-
-      /*<Marker
-       draggable={true}
-       onPress={()=>Alert.alert('click marker')}
-       onDrag={()=>Alert.alert("kaydrıma")}
-       onDragEnd={()=>Alert.alert("kaydırma bitti")}
-       title={"212"}
-       description={"AVM"}
-       pinColor={"blue"}
-       //opacity={0.5}
-        coordinate={{
-           latitude: 41.0477744,
-           longitude: 28.8056053,
-           latitudeDelta: 0.015,
-           longitudeDelta: 0.0121,
-       }}/>*/
-
-      /* <Polygon 
-       onPress={()=>Alert.alert("tıklandı")}
-       strokeWidth={3}
-       strokeColor={'red'}
-       fillColor={"yellow"}
-       tappable={true}
-       coordinates={[
-         {latitude:41.051873,longitude:28.809529},
-         {latitude:41.0505525,longitude:28.809171},
-         {latitude:41.051896,longitude:28.812319},
-
-       ]}/>*/
-
-       /*  <Circle 
-       strokeWidth={3} 
-       strokeColor={"red"} 
-       radius={100} 
-       fillColor={"purple"}
-       center={{latitude:41.0505525,longitude:28.809171}}
-       />*/
-
-
-         /*//Direction (Farturalandırma)
-      <MapViewDirections 
-      apikey={"AIzaSyAWUYge6_XjqqxENGxDaqNmjCYCUeQ7nrU"}
-      origin={{latitude:41.040058,longitude:228.8094457}}
-      destination={{latitude:41.050725,longitude:28.808352}}>
-      </MapViewDirections>
-      */
-     
-      //Dragable Marker
-       /*<Marker
-       draggable={true}
-       onDrag={(e)=>console.log(e.nativeEvent.coordinate)}
-       onDragStart={(e)=>console.log(`start ${e.nativeEvent.coordinate}`)}
-       //onDragEnd={(e)=>console.log(`end:${e.nativeEvent.coordinate}`)}
-       onDragEnd={(e)=>Alert.alert(e.nativeEvent.coordinate.latitude)}
-       title={"Dragable"}
-       description={"Marker"}
-        coordinate={{
-           latitude:41.051873,
-           longitude: 28.809529,
-           latitudeDelta: 0.015,
-           longitudeDelta: 0.0121,
-       }}/>*/
-
-
-       /*       
-       <Marker
-       //Custom Marker
-       title={"Custom"}
-       description={"Marker"}
-        coordinate={{
-           latitude:41.051896,
-           longitude: 28.8056058,
-           latitudeDelta: 0.015,
-           longitudeDelta: 0.0121,
-       }}>
-         <View style={{justifyContent:'center',alignItems:'center',borderRadius:50,width:40,height:40,backgroundColor:'red'}}>
-           <Text style={{color:'white',padding:10,fontSize:15}}>8</Text>
-         </View>
-       </Marker>*/
-
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    height: '100%',
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
- });
-
 
 
